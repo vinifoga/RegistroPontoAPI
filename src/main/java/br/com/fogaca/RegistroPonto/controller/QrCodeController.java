e6a7bfa995180a3c55fa5621d97de9fffd1b44d1
Vinicius Fogaca <viniciusfernandes38@gmail.com>
2022-03-16 05:49:25 -0300

    AutoLoad and Generated QRCode

--- src/main/java/br/com/fogaca/RegistroPonto/controller/QrCodeController.java
+++ src/main/java/br/com/fogaca/RegistroPonto/controller/QrCodeController.java
@@ -1,15 +1,25 @@
 package br.com.fogaca.RegistroPonto.controller;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import br.com.fogaca.RegistroPonto.model.StatusCorrecaoRegistro;
 
 @Controller
 public class QrCodeController {
 
 	@GetMapping("/qrcode")
 	public ModelAndView novo() {
+		DateTimeFormatter hr = DateTimeFormatter.ofPattern("hh:mm:ss");
 		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("data", LocalDate.now());
+		modelAndView.addObject("hora", LocalDateTime.now().format(hr));
+		modelAndView.addObject("status", StatusCorrecaoRegistro.NORMAL.toString());
 		modelAndView.setViewName("/qrcode");
 		return modelAndView;
 	}
