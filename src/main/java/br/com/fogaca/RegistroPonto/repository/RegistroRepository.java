06ae485a9fc956545a24f2e46d8d8b964562bc2f
Vinicius Fernandes Fogaça <viniciusfernandes38@gmail.com>
2022-05-25 22:00:22 -0300

    Native query to list not normal punch

--- src/main/java/br/com/fogaca/RegistroPonto/repository/RegistroRepository.java
+++ src/main/java/br/com/fogaca/RegistroPonto/repository/RegistroRepository.java
@@ -21,6 +21,6 @@ public interface RegistroRepository extends JpaRepository<Registro, Long>{
 	@Query(value = "select * from registro where colaborador_matricula = :colaboradorId and data between :dataInicio and :dataFim", nativeQuery = true)
 	List<Registro> findByIntervaloAndMatricula(LocalDate dataInicio, LocalDate dataFim, Long colaboradorId);
 	
-	@Query(value = "select * from registro where status != 'NORMAL'")
+	@Query(value = "select * from registro where status != 'NORMAL'", nativeQuery = true)
 	List<Registro> findNaoNormal();
 }
